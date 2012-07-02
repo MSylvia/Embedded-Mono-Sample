15ddbd44cd27c727b401204aaca1982a7d06105f
Filip Lundgren <filip@inkdev.net>
2012-07-02 14:54:05 +0200

    Added JIT test case; freezes compiler.

--- Managed Source/ClassLibrary/EntryPoint.cs
+++ Managed Source/ClassLibrary/EntryPoint.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+using System.CodeDom.Compiler;
+
 namespace ClassLibraryNamespace
 {
     public class ClassLibraryManager
@@ -11,6 +14,38 @@ namespace ClassLibraryNamespace
         public ClassLibraryManager()
         {
             System.Console.WriteLine("sup.");
+
+            TestJIT();
+        }
+
+        public void TestJIT()
+        {
+            var compilerParameters = new CompilerParameters();
+
+            compilerParameters.GenerateExecutable = false;
+
+            // Necessary for stack trace line numbers etc
+            compilerParameters.IncludeDebugInformation = true;
+            compilerParameters.GenerateInMemory = true;
+
+            var scriptsDirectory = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, "Scripts");
+
+            Console.WriteLine(scriptsDirectory);
+
+            var scripts = new List<string>();
+            foreach (var script in Directory.GetFiles(scriptsDirectory, "*.cs", SearchOption.AllDirectories))
+            {
+                Console.WriteLine(script);
+                scripts.Add(script);
+            }
+
+            Console.WriteLine("Compiling");
+
+            CompilerResults results;
+            using (var provider = CodeDomProvider.CreateProvider("csharp"))
+                results = provider.CompileAssemblyFromFile(compilerParameters, scripts.ToArray());
+
+            Console.WriteLine("done compiling, assembly is {0}", results.CompiledAssembly != null ? "OK" : "invalid!");
         }
     }
 }
